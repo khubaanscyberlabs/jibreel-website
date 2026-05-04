d4f5dbb88b9a62fe639dbd75398e98c997046233
khubaanscyberlabs <inshaalafzi@gmail.com>
2026-05-04 15:38:45 +0530

    Update product-details.js

--- product-details.js
+++ product-details.js
@@ -1,1 +1,10 @@
-
+const productDetails = {
+  "Impression of Amber Oud - Al Haramain": {
+    topNotes: "Bergamot, Green Notes",
+    middleNotes: "Melon, Pineapple, Sweet Notes, Amber",
+    baseNotes: "Woody Notes, Vanilla, Musk",
+    scentStory: "Amber Oud Gold by Al Haramain opens with a vibrant burst of zesty bergamot and fresh green notes, evoking the luxurious sparkle of golden freshness.",
+    sprayUsageTips: "For the best experience, hold the bottle upright about 10 cm away from your body and press the atomizer with gentle, steady pressure. Spray directly onto your skin or clothes, and avoid spraying near your face. For Fresh fragrances, 8 to 10 sprays will give you the perfect projection, and for Oud and Woody fragrances, 10 to 12 sprays will bring out their full, rich performance. Enjoy every spritz! 🌸",
+    rollonUsageTips: "Apply a small amount directly onto pulse points such as wrists, behind the ears, neck, and inner elbows. Do not rub harshly after applying. Let the oil settle naturally for a smoother and longer-lasting scent experience."
+  }
+};
